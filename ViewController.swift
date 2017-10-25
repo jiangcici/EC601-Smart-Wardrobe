f1fabbdbc76e456f2d775855e953097310cd12c2
Chuan X. Zheng (Tommy) <czheng78@bu.edu>
2017-10-25 12:56:08 -0400

    UI
    -Main page, Suggestion page, Add image page, Inventory page

--- ViewController.swift
+++ ViewController.swift
@@ -12,14 +12,33 @@ class ViewController: UIViewController , UICollectionViewDelegate, UICollectionV
 
     @IBOutlet weak var MyCollectionView: UICollectionView!
     
-    var images = ["Top", "Pant"]
+    var items = ["Top", "Pant"]
+    //var items = [Items]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.MyCollectionView.delegate = self
         self.MyCollectionView.dataSource = self
+        
+        //loadSampleItems()
     }
+    
+    //private func loadSampleMeals() {
+        
+        //let photo1 = UIImage(named: "Top")
+        //let photo2 = UIImage(named: "Pant")
+        
+        //guard let item1 = Items(name: "Hoodies", detail: "A gift from a friend!",photo: photo1) else {
+         //   fatalError("Unable to instantiate meal1")
+       // }
+        
+       // guard let item2 = Items(name: "Jean", detail: "Love it!", photo: photo2) else {
+        //    fatalError("Unable to instantiate meal2")
+       // }
+        
+       // items += [item1, item2]
+    //}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -27,13 +46,13 @@ class ViewController: UIViewController , UICollectionViewDelegate, UICollectionV
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return images.count
+        return items.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collection_cell", for: indexPath) as! MyCollectionViewCell
         // Set images
-        cell.myImageView.image = UIImage(named: images[indexPath.row])
+        cell.myImageView.image = UIImage(named: items[indexPath.row])
         return cell
     }
 }
