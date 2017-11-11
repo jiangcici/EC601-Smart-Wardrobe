ea668c47eb94359d2e25733a0340d5d86f917747
Tommy Zheng <czheng78@bu.edu>
2017-11-11 12:34:29 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/jiangcici/EC601-Smart-Wardrobe""
    This reverts commit 2daf1a8c78b8f7d4584c8cf5548b871feeb293fe.

--- 601wardrobe 2/601wardrobe/Item.swift
+++ 601wardrobe 2/601wardrobe/Item.swift
@@ -5,11 +5,12 @@
 //  Created by 文淦泉 on 2017/10/25.
 //  Copyright © 2017年 Tommy Zheng. All rights reserved.
 //
-
 import Foundation
 import UIKit
+import os.log
 
-class Item {
+class Item: NSObject, NSCoding {
+    
     
     //MARK: Properties
     var name: String
@@ -17,6 +18,19 @@ class Item {
     var detail: String
     var pref: Double = 5   //preference, default value is 5
     var type: String
+    
+    //MARK: Archiving Paths
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("items")
+    
+    //MARK: Types
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let detail = "detail"
+        static let pref = "pref"
+        static let type = "type"
+    }
     
     //MARK: Initialization
     init? (name: String, photo: UIImage?, detail: String, pref: Double, type: String){
@@ -38,4 +52,38 @@ class Item {
         self.pref = pref
         self.type = type
     }
+    
+    //MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(detail, forKey: PropertyKey.detail)
+        aCoder.encode(pref, forKey: PropertyKey.pref)
+        aCoder.encode(type, forKey: PropertyKey.type)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        
+        // The name is required. If we cannot decode a name string, the initializer should fail.
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        // Because photo is an optional property of Meal, just use conditional cast.
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        
+        let detail = aDecoder.decodeObject(forKey: PropertyKey.detail) as? String
+        
+        let pref = aDecoder.decodeFloat(forKey: PropertyKey.pref)
+        
+        let type = aDecoder.decodeObject(forKey: PropertyKey.type) as? String
+        
+        
+        
+        // Must call designated initializer.
+        self.init(name: name, photo: photo, detail: detail!, pref: Double(pref), type: type!)
+        
+    }
+    
 }
