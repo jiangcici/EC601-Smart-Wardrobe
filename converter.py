26ed81bccd91b2f3d9f1e4f42a441b24ed1d6eb7
Chuan X. Zheng (Tommy) <czheng78@bu.edu>
2017-11-12 18:36:56 -0500

    updated converter.py file

--- converter.py
+++ converter.py
@@ -1,4 +1,3 @@
-import coremltools   
-import h5py 
-coreml_model = coremltools.converters.keras.convert('./keras_cnn_model_our_dataset.h5',input_names='image',image_input_names = 'image',output_names='output',class_labels = output_labels, is_bgr=False)   
-coreml_model.save('fashion.mlmodel') 
+import coremltools
+coreml_model = coremltools.converters.keras.convert('keras_cnn_model_our_dataset.h5', input_names='data', image_input_names='data', is_bgr=True, output_names='species')
+coreml_model.save('modelclothings.mlmodel')
