e3c3e69b3b33fdd5e3ee22eb06f356a04810ccb8
Tommy Zheng <czheng78@bu.edu>
2017-12-04 13:14:56 -0500

    Added Weather API

--- 601wardrobe 2/601wardrobe/SignOutCode.swift
+++ 601wardrobe 2/601wardrobe/SignOutCode.swift
@@ -14,12 +14,27 @@ import FirebaseDatabase
 
 class SignOutCode: UIViewController {
 
+    @IBOutlet weak var cityNameTF: UITextField!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var cityTempLabel: UILabel!
+    @IBOutlet weak var cityTempLabel2: UILabel!
+    
+    
+    private let openWeatherMapBaseURL = "http://api.openweathermap.org/data/2.5/weather"
+    private let openWeatherMapAPIKey = "f22eb93db195b138f3e28fd4325b43b1"
+    
+    
+    @IBAction func refreshClicked(_ sender: Any) {
+        let newstring = cityNameTF.text!.replacingOccurrences(of: " ", with: "%20")
+        getWeatherData(urlString: "http://api.openweathermap.org/data/2.5/weather?q=" + newstring + ",US&APPID=f22eb93db195b138f3e28fd4325b43b1")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        getWeatherData(urlString: "http://api.openweathermap.org/data/2.5/weather?q=Boston,US&APPID=f22eb93db195b138f3e28fd4325b43b1")
     }
-
+    
+    // Sign Out button Function
     @IBAction func SignOut (_ sender: Any) {
         let firebaseAuth = Auth.auth()
         do {
@@ -32,5 +47,40 @@ class SignOutCode: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    // Get Weather Data from OPenWeather API
+    func getWeatherData(urlString: String) {
+        let url = NSURL(string: urlString)
+        let task = URLSession.shared.dataTask(with: url! as URL) {(data, response, error) in
+            DispatchQueue.global(qos: .background).async {
+                DispatchQueue.main.async {
+                    self.setLabels(weatherData: data! as NSData)
+                }
+            }
+        }
+        task.resume()
+    }
+    
+
+    var jsonData: AnyObject?
+    var temp2: Double?
+    var temp3: Double?
+    func setLabels(weatherData: NSData) {
+        do {
+            self.jsonData = try JSONSerialization.jsonObject(with: weatherData as Data, options: []) as! NSDictionary
+        } catch {
+            print("Error!")
+        }
+        if let name = jsonData!["name"] as? String {
+            cityNameLabel.text = "\(name)"
+        }
+        if let main = jsonData!["main"] as? NSDictionary {
+            if let temp = main["temp"] as? Double {
+                self.temp2 = ((temp*9)/5)-459.67
+                self.temp3 = temp-273.15
+                cityTempLabel.text = String(format: "%.1f", temp2!) + String("  F")
+                cityTempLabel2.text = String(format: "%.1f", temp3!) + String("  C")
+            }
+        }
+    }
 
 }
--- 601wardrobe 2/601wardrobe/UIImage_CVPixelBuffer.swift
+++ 601wardrobe 2/601wardrobe/UIImage_CVPixelBuffer.swift
@@ -10,161 +10,38 @@ import UIKit
 
 extension UIImage {
 
-    var buffer: CVPixelBuffer? {
+    public func pixelBufferGray(width: Int, height: Int) -> CVPixelBuffer? {
 
-        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let attributes = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue]
 
-        var pixelBuffer: CVPixelBuffer?
-        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(self.size.width), Int(self.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(width), Int(height), kCVPixelFormatType_OneComponent8, attributes as CFDictionary, &pixelBuffer)
 
-        guard status == kCVReturnSuccess else {
+        guard status == kCVReturnSuccess, let imageBuffer = pixelBuffer else {
             return nil
         }
 
-        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        CVPixelBufferLockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
 
-        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-        let context = CGContext(data: pixelData, width: Int(self.size.width), height: Int(self.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        let imageData =  CVPixelBufferGetBaseAddress(imageBuffer)
 
-        context?.translateBy(x: 0, y: self.size.height)
-        context?.scaleBy(x: 1.0, y: -1.0)
+        guard let context = CGContext(data: imageData, width: Int(width), height:Int(height),
+                                      bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(imageBuffer),
+                                      space: CGColorSpaceCreateDeviceGray(),
+                                      bitmapInfo: CGImageAlphaInfo.none.rawValue) else {
+                                        return nil
+        }
 
-        UIGraphicsPushContext(context!)
-        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        context.translateBy(x: 0, y: CGFloat(height))
+        context.scaleBy(x: 1, y: -1)
+
+        UIGraphicsPushContext(context)
+        self.draw(in: CGRect(x:0, y:0, width: width, height: height) )
         UIGraphicsPopContext()
-        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        CVPixelBufferUnlockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
 
-        return pixelBuffer
-    }
+        return imageBuffer
 
-    var grayscaledBuffer: CVPixelBuffer? {
-
-        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
-
-        var pixelBuffer: CVPixelBuffer?
-        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(self.size.width), Int(self.size.height), kCVPixelFormatType_OneComponent8, attrs, &pixelBuffer)
-
-        guard (status == kCVReturnSuccess) else {
-            return nil
-        }
-
-        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
-
-        let grayColorSpace = CGColorSpaceCreateDeviceGray()
-        let context = CGContext(data: pixelData, width: Int(self.size.width), height: Int(self.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: grayColorSpace, bitmapInfo: CGImageAlphaInfo.none.rawValue)
-
-        context?.translateBy(x: 0, y: self.size.height)
-        context?.scaleBy(x: 1.0, y: -1.0)
-
-        UIGraphicsPushContext(context!)
-        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
-        UIGraphicsPopContext()
-        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-
-        return pixelBuffer
-    }
-
-    func resize(size: CGSize) -> UIImage? {
-
-        UIGraphicsBeginImageContext(CGSize(width: size.width, height: size.height))
-        self.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return newImage
-    }
-
-    var grayscaled: UIImage? {
-
-        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-
-        let context = CGContext(data: nil,
-                                width: Int(size.width),
-                                height: Int(size.height),
-                                bitsPerComponent: 8,
-                                bytesPerRow: 0,
-                                space: CGColorSpaceCreateDeviceGray(),
-                                bitmapInfo: CGImageAlphaInfo.none.rawValue)
-
-        guard let ctx = context, let cgImage = cgImage else {
-
-            return nil
-        }
-
-        ctx.draw(cgImage, in: rect)
-
-        guard let image = ctx.makeImage() else { return nil }
-
-        return UIImage(cgImage: image)
-    }
-
-    func grayscaledPixels() -> [CGFloat]? {
-
-        guard let cgImage = self.cgImage else { return nil }
-
-        let size     = self.size
-        let dataSize  = size.width * size.height * 4
-        var pixelData = [UInt8](repeating: 0, count: Int(dataSize))
-
-        let colorSpace = CGColorSpaceCreateDeviceRGB()
-        let context    = CGContext(data: &pixelData,
-                                   width: Int(size.width),
-                                   height: Int(size.height),
-                                   bitsPerComponent: 8,
-                                   bytesPerRow: 4 * Int(size.width),
-                                   space: colorSpace,
-                                   bitmapInfo: CGImageAlphaInfo.noneSkipLast.rawValue)
-
-        context?.draw(cgImage, in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-
-        var result: [CGFloat] = []
-
-        for i in stride(from: 0, to: pixelData.count, by: 4) {
-
-            let val = (CGFloat(pixelData[i]) + CGFloat(pixelData[i+1]) + CGFloat(pixelData[i+2])) / (255.0 * 3.0)
-
-            result.append(val)
-        }
-
-        return result
     }
 }
-
-//extension UIImage {
-//
-//    public func pixelBufferGray(width: Int, height: Int) -> CVPixelBuffer? {
-//
-//        var pixelBuffer : CVPixelBuffer?
-//        let attributes = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue]
-//
-//        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(width), Int(height), kCVPixelFormatType_OneComponent8, attributes as CFDictionary, &pixelBuffer)
-//
-//        guard status == kCVReturnSuccess, let imageBuffer = pixelBuffer else {
-//            return nil
-//        }
-//
-//        CVPixelBufferLockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
-//
-//        let imageData =  CVPixelBufferGetBaseAddress(imageBuffer)
-//
-//        guard let context = CGContext(data: imageData, width: Int(width), height:Int(height),
-//                                      bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(imageBuffer),
-//                                      space: CGColorSpaceCreateDeviceGray(),
-//                                      bitmapInfo: CGImageAlphaInfo.none.rawValue) else {
-//                                        return nil
-//        }
-//
-//        context.translateBy(x: 0, y: CGFloat(height))
-//        context.scaleBy(x: 1, y: -1)
-//
-//        UIGraphicsPushContext(context)
-//        self.draw(in: CGRect(x:0, y:0, width: width, height: height) )
-//        UIGraphicsPopContext()
-//        CVPixelBufferUnlockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
-//
-//        return imageBuffer
-//
-//    }
-//}
 
