acec8854c8d89fe18ca34ebcf630f77fc02e1517
Tommy Zheng <czheng78@bu.edu>
2017-12-05 00:09:26 -0500

    Added SuggestionVC.swift

---
+++ 601wardrobe 2/601wardrobe/suggestionVC.swift
@@ -0,0 +1,48 @@
+//
+//  suggestionVC.swift
+//  601wardrobe
+//
+//  Created by Tommy Zheng on 12/4/17.
+//  Copyright © 2017 Tommy Zheng. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import os.log
+
+class suggestionVC: UIViewController {
+    
+    var wardrobe = [Item]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        assignArray()
+        //getTempF()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+//    func getTempF() {
+//        let otherVC = SignOutCode()
+//        let tempF = otherVC.temp2
+//        print(tempF)
+//    }
+    
+    func assignArray() {
+        wardrobe = loadItems()!
+        print(wardrobe[0].type)
+    }
+    
+    
+    // Fetech wardrobe items from Database folder
+    private func loadItems() -> [Item]?  {
+        return NSKeyedUnarchiver.unarchiveObject(withFile: Item.ArchiveURL.path) as? [Item]
+    }
+    
+}
